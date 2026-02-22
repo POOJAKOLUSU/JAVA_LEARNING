d3f16a2da2cec857f8277651c49b928b578dc3c1
KOLUSU POOJA <kolusupooja22@gmail.com>
2026-02-22 18:10:50 +0530

    printing in java

---
+++ Printing.java
@@ -0,0 +1,12 @@
+public class Printing{
+    public static void main(String [] args){
+        System.out.print(4+2);
+        System.out.print(8+4);
+    
+        //here the output gets printed on the same line ,so if we use System.ou.println then the output will be decalred on new seperate line 
+      
+      System.out.println();
+        System.out.println("pooja");
+        System.out.println(8+4);
+    }
+}
