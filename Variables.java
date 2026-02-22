36a8607a2755240f56bbdbb23631812cb3433ce0
KOLUSU POOJA <kolusupooja22@gmail.com>
2026-02-22 18:34:21 +0530

    Variables in java

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String [] args){
+        int num1=30;
+        int num2=29;
+        int result = num1+num2;
+        System.out.println(result);
+// A variable is: A named memory location used to store data that can change during program execution.
+// here num1, num2, result ,represents a variable and int represents the data type and equal to is the assignment operator and 30 is some value.
+    }
+}
